df74a030cfd656b351d6a59cac2b106c5b349806
Joe <yousefibrahem9999@gmail.com>
2025-04-03 14:55:57 +0300

    configuring Swagger

--- E-Commerce.WebAPI/Program.cs
+++ E-Commerce.WebAPI/Program.cs
@@ -6,12 +6,22 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+#region Swagger Config
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+#endregion
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
